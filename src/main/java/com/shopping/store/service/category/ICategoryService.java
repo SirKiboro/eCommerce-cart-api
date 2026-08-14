95009d0202b1c5b4e920ad33a46dfab566c9e1c9
Paul Kamau <paulkiboro58@gmail.com>
2026-08-14 11:00:07 +0300

    feat: Implemented add, update, delete and all get methods for CategoryService and run time error handling

--- src/main/java/com/shopping/store/service/category/ICategoryService.java
+++ src/main/java/com/shopping/store/service/category/ICategoryService.java
@@ -1,4 +1,16 @@
 package com.shopping.store.service.category;
 
+import com.shopping.store.model.Category;
+
+import java.util.List;
+
 public interface ICategoryService {
+
+    Category getCategoryById(Long id);
+    Category getCategoryByName(String name);
+    Category addCategory(Category category);
+    Category updateCategory(Category category, Long id);
+    List<Category> getAllCategories();
+    void deleteCategoryById(Long id);
+
 }
