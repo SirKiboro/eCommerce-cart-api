737834a932b2568d442836a59022081376b4d55b
Paul Kamau <paulkiboro58@gmail.com>
2026-08-17 12:57:32 +0300

    feat: Implement getAll, and addCategory endpoints in CategoryController

--- src/main/java/com/shopping/store/controller/CategoryController.java
+++ src/main/java/com/shopping/store/controller/CategoryController.java
@@ -1,4 +1,48 @@
 package com.shopping.store.controller;
 
+import com.shopping.store.exceptions.AlreadyExistsException;
+import com.shopping.store.model.Category;
+import com.shopping.store.response.ApiResponse;
+import com.shopping.store.service.category.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+
+    private final ICategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories(){
+
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found! ",categories));
+
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error:",INTERNAL_SERVER_ERROR));
+
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category name){
+
+        try {
+            Category theCategory = categoryService.addCategory(name);
+            return ResponseEntity.ok(new ApiResponse("Success!", theCategory));
+
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
 }
