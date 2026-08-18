b1d21c8474513ccd0b9074482376af10754c8a10
Paul Kamau <paulkiboro58@gmail.com>
2026-08-18 08:58:07 +0300

    feat: Implement getMapping for getAllProducts in ProductController

--- src/main/java/com/shopping/store/controller/ProductController.java
+++ src/main/java/com/shopping/store/controller/ProductController.java
@@ -1,4 +1,39 @@
 package com.shopping.store.controller;
 
+import com.shopping.store.exceptions.ResourceNotFoundException;
+import com.shopping.store.model.Product;
+import com.shopping.store.response.ApiResponse;
+import com.shopping.store.service.product.IProductService;
+import jakarta.annotation.Resource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/products")
 public class ProductController {
+
+    private final IProductService productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllProducts(){
+
+        try {
+            List<Product> products = productService.getAllProducts();
+            return ResponseEntity.ok(new ApiResponse("Found ", products));
+
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error!",INTERNAL_SERVER_ERROR));
+
+        }
+    }
 }
